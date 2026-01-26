a64f6e2eb9438095d82189985d5d526fe502c23b
Enyinna Great <111206274+Greezman123@users.noreply.github.com>
2026-01-26 12:51:53 +0100

    Update sessionService.js

--- LetStudy/app/assets/js/sessionService.js
+++ LetStudy/app/assets/js/sessionService.js
@@ -7,31 +7,16 @@ const API_URL = 'https://letstudy.infinityfreeapp.com/session.php';
  * @param {string} varName - The key of the session variable.
  */
  
-async function getSessionValue(varName) {
-    const formData = new FormData();
-    formData.append('key', varName);
-
-    try {
-        const response = await fetch(API_URL, {
-            method: 'POST',
-            body: formData,
-            credentials: 'include'
-        });
-
-        if (!response.ok) {
-            throw new Error(`Server error: ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        if (!data.success) {
-            console.warn(`Session Notice: ${data.error}`);
-            return null;
-        }
-
-        return data.value;
-    } catch (error) {
-        console.error('Session Fetch Failure:', error);
-        throw error;
+async function getSessionValue() {
+    fetch(API_URL, {
+    credentials: 'include' // important to send PHP cookies
+})
+.then(res => res.json())
+.then(data => {
+    if (data.loggedIn) {
+        console.log("Welcome", data.user.username);
+    } else {
+        console.log("Not logged in");
     }
+});
 }
